eb19bd22d27ff6b240611765102c53dcad15a784
Buyantogtokh Khurelbaatar <vicekurel@gmail.com>
2024-04-08 21:14:53 -0700

    updated files and image sources added

--- scripts.js
+++ scripts.js
@@ -1,94 +1,268 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- * 
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- * 
- */
+let dbz = [
+{
+    name: "Goku",
+    race: "Saiyan",
+    gender: "Male",
+    "bio": "Goku is a Saiyan originally sent to Earth as an infant with the mission to destroy its people. However he grew up to become Earth's greatest defender. He constantly strives and trains to be the greatest warrior possible, which has kept the Earth and the universe safe from destruction many times.",
+    health: 500000,
+    attack: 50000,
+    abilities: [
+    "Super Saiyan Transformations",
+    "Super Kamehameha",
+    "Super Dragon Fist"
+    ],
+    photo: "images/goku.gif"
+},
+{
+    name: "Vegeta",
+    race: "Saiyan",
+    gender: "Male",
+    bio: "Vegeta is the prince of the fallen Saiyan race. He is the eldest son of King Vegeta. the husband of Bulma, the father of Trunks",
+    health: 480000,
+    attack: 48000,
+    abilities: [
+    "Super Saiyan Transformations",
+    "Final Flash",
+    "Big Bang Attack"
+    ],
+    photo: "images/vegeta2.gif"
+},
+{
+    name: "Gohan",
+    race: "Saiyan",
+    gender: "Male",
+    bio: "Gohan is son of Goku. He is named after Goku's adoptive grandfather, Gohan. During the battle with Cell, Gohan became the strongest fighter through the Super Saiyan 2 transformation. In this form, Gohan surpassed Cell and even his father in terms of power and speed.",
+    health: 470000,
+    attack: 47500,
+    abilities: [
+    "Super Saiyan Transformations",
+    "Super Kamehameha",
+    "Masenko"
+    ],
+    photo: "images/gohan.gif"
+},
+{
+    name: "Trunks",
+    race: "Saiyan",
+    gender: "Male",
+    bio: "Trunks is Vegeta's son. Several characters, such as Goku, Krillin and Bulma, remark how much Trunks resembles Vegeta",
+    health: 440000,
+    attack: 44000,
+    abilities: [
+    "Super Saiyan Transformations",
+    "Galic Gun",
+    "Finish Buster"],
+    photo: "images/trunks.gif"
+},
+{
+    name: "Frieza",
+    race: "Unknown",
+    gender: "Male",
+    bio: "Frieza was the emperor of the universe, who controlled his own imperial army and was feared for his ruthlessness and power, but was taken out of power when he was killed by Future Trunks & Goku.",
+    health: 375000,
+    attack: 37000,
+    "abilities": [
+    "Eye Laser",
+    "Death Beam",
+    "Death Ball"
+    ],
+    photo: "images/frieza.jpeg"
+},
+{
+    name: "Beerus",
+    race: "God",
+    gender: "Male",
+    bio: "Beerus is the God of Destruction of Universe 7. He is accompanied by his martial arts teacher and attendant, Whis. Beerus' twin brother is Champa, the God of Destruction of Universe 6.",
+    health: 999999999,
+    attack: 999999999,
+    abilities: [
+    "Destruction Sphere",
+    "Destruction Gods, Fierce Attack",
+    "Hakai"
+    ],
+    photo: "images/beerus.jpeg"
+},
+{
+    name: "Piccolo",
+    race: "Namekian",
+    gender: "Male",
+    bio: "Piccolo Jr., usually just called Piccolo is a Namekian and also the final child and reincarnation of the Demon King Piccolo, later becoming the reunification of the Nameless Namekian after fusing with Kami.",
+    health: 230000,
+    attack: 25000,
+    abilities: [
+    "Special Beam Cannon",
+    "Tri-Form",
+    "Light Grenade"
+    ],
+    photo: "images/piccolo.gif"
+},
+{
+    name: "Bulma",
+    race: "Human",
+    gender: "Female",
+    bio: "Bulma is a brilliant scientist and the second daughter of Capsule Corporation's founder Dr. Brief and is Goku's first friend. Also Vegeta's wife.",
+    health: 100,
+    attack: 50,
+    abilities: [
+        "Technology",
+        "Slapping"
+    ],
+    photo: "images/bulma.jpeg"
+},
+{
+    name: "Android 17",
+    race: "Unknown",
+    gender: "Male",
+    bio: "Android 17, Lapis when he was an ordinary Human, is the twin brother of Android 18 and Dr. Gero's 17th android creation, a cyborg due to being a human at one point, and one of his first human subjects, designed to serve Gero's vendetta against Goku",
+    health: 200000,
+    attack: 25000,
+    abilities: [
+        "Super Strength",
+        "Super Speed",
+        "Unlimited Energy"
+    ],
+    photo: "images/android17.jpeg"
+},
+{
+    name: "Android 18",
+    race: "Unknown",
+    gender: "Female",
+    bio: "Android 18 , Lazuli when she was an ordinary human, is the older twin sister of Android 17 and Dr. Gero's eighteenth android creation, designed to serve Gero's vendetta against Goku. ",
+    health: 200000,
+    attack: 25000,
+    abilities: [
+        "Super Strength",
+        "Super Speed",
+        "Unlimited Energy"
+    ],
+    photo: "images/android18.jpeg"
+},
+{
+    name: "Mr Satan",
+    race: "Human",
+    gender: "Male",
+    bio: "Mr Satan, a great fighter, champion of the world, cheered by all humanity. In reality he hides a clumsy and cowardly personality.",
+    health: 200,
+    attack: 100,
+    abilities: [
+        "Stronger and faster than average human"
+    ],
+    photo: "images/satan.jpeg"
+},
+{
+    name: "Videl",
+    race: "Human",
+    gender: "Female",
+    bio: "Videl, a martial artist, daughter of Mr Satan. She is also the wife of Gohan. In her spare time, Videl helps the police and fights crime",
+    health: 150,
+    attack: 120,
+    abilities: [
+        "Stronger and faster than average human",
+        "Flying"
+    ],
+    photo: "images/videl.png"   
+}]
 
 
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+document.addEventListener("DOMContentLoaded", function() {
+    const characterList = document.getElementById("characterList");
+    const searchInput = document.getElementById("searchInput");
+    const sortBy = document.getElementById("sortBy");
+    const categoryButtons = document.querySelectorAll(".categoryButton");
 
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
-];
-// Your final submission should have much more data than this, and 
-// you should use more than just an array of strings to store it all.
+    let filteredCharacters = [...dbz];
 
-
-// This function adds cards the page to display the data in the array
-function showCards() {
-    const cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = "";
-    const templateCard = document.querySelector(".card");
-    
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
-
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
-
-        const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
-        cardContainer.appendChild(nextCard); // Add new card to the container
+    function generateCharacterCards(characters) {
+        characterList.innerHTML = "";
+        characters.forEach(character => {
+            const card = document.createElement("div");
+            card.classList.add("characterCard");
+            const photoContainer = document.createElement("div");
+            photoContainer.classList.add("photoContainer");
+            const img = document.createElement("img");
+            img.src = character.photo;
+            img.alt = character.name + " Photo";
+            photoContainer.appendChild(img);
+            card.appendChild(photoContainer);
+            card.innerHTML += `
+                <h2>${character.name}</h2>
+                <p>Race: ${character.race}</p>
+                <p>Gender: ${character.gender}</p>
+                <p>Health: ${character.health}</p>
+                <p>Attack: ${character.attack}</p>
+                <p>Bio: ${character.bio}</p>
+                <p>Abilities: ${character.abilities.join(", ")}</p>
+                <button class="deleteButton" data-name="${character.name}">Delete</button>
+            `;
+            characterList.appendChild(card);
+        });
+        attachDeleteListeners();
     }
-}
 
-function editCardContent(card, newTitle, newImageURL) {
-    card.style.display = "block";
+    generateCharacterCards(filteredCharacters);
 
-    const cardHeader = card.querySelector("h2");
-    cardHeader.textContent = newTitle;
+    searchInput.addEventListener("input", function() {
+        const searchTerm = searchInput.value.toLowerCase();
+        filteredCharacters = dbz.filter(character =>
+            character.name.toLowerCase().startsWith(searchTerm)
+        );
+        applyFiltersAndSort();
+    });
 
-    const cardImage = card.querySelector("img");
-    cardImage.src = newImageURL;
-    cardImage.alt = newTitle + " Poster";
+    sortBy.addEventListener("change", function() {
+        applyFiltersAndSort();
+    });
 
-    // You can use console.log to help you debug!
-    // View the output by right clicking on your website,
-    // select "Inspect", then click on the "Console" tab
-    console.log("new card:", newTitle, "- html: ", card);
-}
+    categoryButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            const category = button.id;
+            if (category === "all") {
+                filteredCharacters = [...dbz];
+            } else {
+                filteredCharacters = dbz.filter(character =>
+                    (category === "male" && character.gender.toLowerCase() === "male") ||
+                    (category === "female" && character.gender.toLowerCase() === "female") ||
+                    (category === character.race.toLowerCase())
+                );
+            }
+            applyFiltersAndSort();
+        });
+    });
 
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
+    function applyFiltersAndSort() {
+        const sortCriterion = sortBy.value;
+        let sortedCharacters = [...filteredCharacters];
+        switch (sortCriterion) {
+            case "healthAsc":
+                sortedCharacters.sort((a, b) => a.health - b.health);
+                break;
+            case "healthDesc":
+                sortedCharacters.sort((a, b) => b.health - a.health);
+                break;
+            case "attackAsc":
+                sortedCharacters.sort((a, b) => a.attack - b.attack);
+                break;
+            case "attackDesc":
+                sortedCharacters.sort((a, b) => b.attack - a.attack);
+                break;
+        }
+        generateCharacterCards(sortedCharacters);
+    }
 
-function quoteAlert() {
-    console.log("Button Clicked!")
-    alert("I guess I can kiss heaven goodbye, because it got to be a sin to look this good!");
-}
-
-function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
-    showCards(); // Call showCards again to refresh
-}
+    function attachDeleteListeners() {
+        const deleteButtons = document.querySelectorAll(".deleteButton");
+        deleteButtons.forEach(button => {
+            button.addEventListener("click", function() {
+                const name = button.dataset.name;
+                dbz = dbz.filter(character => character.name !== name);
+                filteredCharacters = filteredCharacters.filter(character => character.name !== name);
+                applyFiltersAndSort();
+    
+                const cardToRemove = button.closest(".characterCard");
+                cardToRemove.remove();
+            });
+        });
+    }
+    
+    
+});
